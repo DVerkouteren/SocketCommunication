d50afe6f9fda7062be9a02d3baac905e33176dbd
Addie Patterson <patteral@rose-hulman.edu>
2025-12-28 16:02:54 -0500

    Working usernames and sending/recieving two-ways

--- SocketsClient.c
+++ SocketsClient.c
@@ -1,63 +1,97 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+/* 
+    SocketsClient.c
+    ECE312-01 Project 1
+    AUTHORS: Dane Verkouteren & Addie Patterson
+    DESCRIPTION: 
+*/
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <strings.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
 #include <netdb.h>
 
-   void error(char *msg)
-   {
-       perror(msg);
-       exit(0);
-   }
+void error(char *msg) { 
+    perror(msg); exit(0); 
+} 
 
-   int main(int argc, char *argv[])
-   {
-     int sockfd, portno, n;
-     struct sockaddr_in serv_addr;
-     struct hostent *server;
-     char buffer[256];
-     if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
-     }
-     portno = atoi(argv[2]);
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-         error("ERROR opening socket");
-     server = gethostbyname(argv[1]);
-     if (server == NULL) {
-         fprintf(stderr,"ERROR, no such host\n");
-         exit(0);
-     }
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     serv_addr.sin_family = AF_INET;
-     bcopy((char *)server->h_addr, 
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
-     serv_addr.sin_port = htons(portno);
-     while(1){
-        if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-            error("ERROR connecting");
-        while(1){
-        printf("Please enter the message: ");
-        bzero(buffer,256);
-        fgets(buffer,255,stdin);
-        n = write(sockfd,buffer,strlen(buffer));
-        if (n < 0) 
-            error("ERROR writing to socket");
-        bzero(buffer,256);
-        n = read(sockfd,buffer,255);
-        if (n < 0) 
-            error("ERROR reading from socket");
-        printf("%s\n",buffer);
-        }
+int main(int argc, char *argv[]) {
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256]; 
+    char username[32]; 
+    char otherUser[32]; 
 
+    if (argc < 3) {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]); 
+        exit(0); 
     }
-     return 0;
-   }
+    portno = atoi(argv[2]); 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 
+    if (sockfd < 0) {
+        error("ERROR opening socket"); 
+    }
+    server = gethostbyname(argv[1]); 
+        
+    if (server == NULL) { 
+        fprintf(stderr,"ERROR, no such host\n"); 
+        exit(0); 
+    } 
+        
+    bzero((char *) &serv_addr, sizeof(serv_addr)); 
+    serv_addr.sin_family = AF_INET; 
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno); 
+    printf("Waiting for connection . . .\n"); 
+
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
+        error("ERROR connecting");
+    }
+
+    while(1) { 
+        printf("Provide user name: "); 
+        fflush(stdout); 
+        fgets(username,32,stdin); 
+        username[strcspn(username, "\n")] = 0; 
+
+        n = write(sockfd, username, strlen(username)); 
+        if (n < 0) {
+            error("ERROR writing username to socket");
+        } 
+
+        bzero(otherUser, 32);
+        n = read(sockfd, otherUser, 31);
+        if (n < 0) {
+            error("ERROR reading server's username from socket");
+        } 
+            
+        while(1) { 
+            printf("<%s> ", username); 
+            fflush(stdout); 
+            
+            bzero(buffer,256); 
+            fgets(buffer,255,stdin); 
+            buffer[strcspn(buffer, "\n")] = 0;
+
+            n = write(sockfd,buffer,strlen(buffer)); 
+            if (n < 0) {
+                error("ERROR writing to socket");
+            }
+
+            bzero(buffer,256); 
+            n = read(sockfd,buffer,255); 
+            if (n < 0) {
+                error("ERROR reading from socket");
+            } 
+            printf("<%s> %s\n", otherUser, buffer); 
+        } 
+    } 
+    return 0; 
+}
--- SocketsServer.c
+++ SocketsServer.c
@@ -1,25 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>     // exit, atoi
-#include <string.h>     // memset
-#include <unistd.h>     // read, write, close
 
+  /* 
+    SocketsServer.c
+    ECE312-01 Project 1
+    AUTHORS: Dane Verkouteren & Addie Patterson
+    DESCRIPTION: 
+  */
+  
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <strings.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
+#include <netdb.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+  void error(char *msg) {
+        perror(msg);
+        exit(1);
+    }
 
-  void error(char *msg)
-  {
-      perror(msg);
-      exit(1);
-  }
-
-   int main(int argc, char *argv[])
-   {
+   int main(int argc, char *argv[]) {
         int sockfd, newsockfd, portno, clilen;
         char buffer[256];
         struct sockaddr_in serv_addr, cli_addr;
         int n;
+        char username[32];
+        char message[512];
+        char clientUsername[256];
+
         if (argc < 2) {
             fprintf(stderr,"ERROR, no port provided\n");
             exit(1);
@@ -32,27 +43,54 @@
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_port = htons(portno);
         serv_addr.sin_addr.s_addr = INADDR_ANY;
-        if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                 sizeof(serv_addr)) < 0) 
-                 error("ERROR on binding");
-        listen(sockfd,5);
-
         while(1){
+            if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+                error("ERROR on binding");
+            listen(sockfd,5);
             clilen = sizeof(cli_addr);
             newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-            if (newsockfd < 0) 
+            if (newsockfd < 0) {
                 error("ERROR on accept");
+            }
+            
+            printf("Provide user name: ");
+            fflush(stdout);
+            fgets(username,32,stdin);
+            username[strcspn(username, "\n")] = 0;
 
-            while(1){
+            bzero(clientUsername, 32);
+            n = read(newsockfd, clientUsername, 31);
+            if (n < 0) {
+                error("ERROR reading client's username from socket");
+            }
+
+            n = write(newsockfd, username, strlen(username));
+            if (n < 0) {
+                error("ERROR writing username to socket");
+            }
+
+            while(1) {
                 bzero(buffer,256);
                 n = read(newsockfd,buffer,255);
-                if (n < 0) error("ERROR reading from socket");
-                printf("Here is the message: %s\n",buffer);
-                n = write(newsockfd,"I got your message",18);
-                if (n < 0) error("ERROR writing to socket");
+                
+                if (n < 0) {
+                    error("ERROR reading from socket");
+                }
+
+                printf("<%s> %s\n", clientUsername, buffer);
+
+                printf("<%s> ", username); 
+                fflush(stdout); 
+
+                bzero(buffer,256); 
+                fgets(buffer,255,stdin); 
+                buffer[strcspn(buffer, "\n")] = 0;
+
+                n = write(newsockfd,buffer,strlen(buffer)); 
+                if (n < 0) {
+                    error("ERROR writing to socket");
+                }
             }
         }
         return 0; 
    }
-
-
